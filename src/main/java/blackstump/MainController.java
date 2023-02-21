eebc8209f19fe84362de13a6cb9c722644473bd4
SAURABHJA1N <123818765+SAURABHJA1N@users.noreply.github.com>
2023-02-21 20:00:17 +1100

    Update MainController.java

--- src/main/java/blackstump/MainController.java
+++ src/main/java/blackstump/MainController.java
@@ -19,7 +19,7 @@ public class MainController {
     }
     @GetMapping("/api")
     public String index(@RequestHeader("X-Api-Version") String apiVersion) {
-        logger.info("Received a request for API version " + apiVersion);
+//         logger.info("Received a request for API version " + apiVersion);
         return "API v3";
     }
 
